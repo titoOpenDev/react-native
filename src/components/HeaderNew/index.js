34e99841709f9718f319367960c34b558f5784d3
Horacio <horacio@tdil.com.ar>
2020-05-22 14:25:56 -0300

    Image added

--- src/components/HeaderNew/index.js
+++ src/components/HeaderNew/index.js
@@ -1,29 +1,28 @@
-import React, {useEffect, useState,} from 'react';
+import React, { useEffect, useState, } from 'react';
 import { Header, Left, Body, Right, Icon, Title } from 'native-base';
 
-import { Ionicons } from '@expo/vector-icons';
-import { Foundation } from '@expo/vector-icons'; 
+import { Ionicons, Foundation } from '@expo/vector-icons';
 
 import styles from "./style";
 
 export default function HeaderNew() {
     return (
-      <Container>
-        <Header style={styles.header} androidStatusBarColor={styles.header}>
-          <Left>
-            <Button transparent>
-              <Ionicons style={styles.leftIcon} name={styles.leftIcon.name} size={styles.leftIcon.size} />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right>
-            <Button transparent>
-              <Foundation style={styles.rightIcon} name={styles.rightIcon.name} size={styles.rightIcon.size} />
-            </Button>
-          </Right>
-        </Header>
-      </Container>
+        <Container>
+            <Header style={styles.header} androidStatusBarColor={styles.header}>
+                <Left style={{ flex: 1 }}>
+                    <Button transparent>
+                        <Ionicons style={styles.leftIcon} name={styles.leftIcon.name} size={styles.leftIcon.size} />
+                    </Button>
+                </Left>
+                <Body style={{ flex: 1, justifyContent: 'center' }}>
+                    <Image source={require('../../../assets/ase_nacional_imagen_app.png')} style={styles.image} />
+                </Body>
+                <Right style={{ flex: 1 }}>
+                    <Button transparent>
+                        <Foundation style={styles.rightIcon} name={styles.rightIcon.name} size={styles.rightIcon.size} />
+                    </Button>
+                </Right>
+            </Header>
+        </Container>
     );
-  }
+}
--- src/components/HeaderNew/style.js
+++ src/components/HeaderNew/style.js
@@ -2,17 +2,15 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     header: {
-        backgroundColor: '#f16820',
-        androidStatusBarColor: '#e45205',
+        backgroundColor: '#f16820', androidStatusBarColor: '#e45205',
     },
     leftIcon: {
-        color: 'white',
-        size: 24,
-        name: 'md-menu',
+        color: 'white', size: 24, name: 'md-menu',
     },
     rightIcon: {
-        color: 'white',
-        size: 24,
-        name: 'info',
-    }
+        color: 'white', size: 24, name: 'info',
+    },
+    image: {
+        width: 64, height: 24, resizeMode: 'stretch', tintColor: 'white',
+    },
 });
