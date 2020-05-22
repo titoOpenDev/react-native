ba1c7944759a9f1c50744fe5c8aeff76683e5261
Juan Martin Di Martino <matias.ciarla@opendevsolutions.com.ar>
2020-05-22 14:53:27 -0300

    resuelto conflictos de merge de forma manual

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -38,18 +38,8 @@ export default function Login({navigation}) {
     },[loginData,error,loged]);
 
     const handleLoginPress = async () => {
-<<<<<<< HEAD
         dispatch(login({username , password}));
         
-=======
-      navigation.navigate(HOME);
-        // dispatch(login({username , password}));
-        // if(loginData) {
-        //     navigation.navigate(HOME);
-        // }else{
-        //     alert(error)
-        // }
->>>>>>> react-native-ducks-001
     };
 
     const handleTouchableOpacity = async () => {
