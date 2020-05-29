a3f19870c502d7b0b54dc42173b0856cd3721fbd
Horacio <horacio@tdil.com.ar>
2020-05-29 11:37:34 -0300

    added missing events

--- src/views/AccountActivation/index.js
+++ src/views/AccountActivation/index.js
@@ -42,44 +42,44 @@ export default function AccountActivation({ navigation }) {
 
   return (
     <View style={{ flex: 1, }}>
-    <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS == "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS == "ios" ? 0 : -100}>
-        <ScrollView style={styles.scrollView}>
-          <View style={{ minHeight: height-Constants.statusBarHeight }}>
-            <View style={{ backgroundColor: '#7a7e7f', justifyContent: 'center', minHeight: 200, }}>
-              <View style={{ alignItems: 'flex-start', top: -20, }}>
-                <Ionicons style={{ marginLeft: 24 }} name="ios-arrow-back" size={24} color="white" />
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS == "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS == "ios" ? 0 : -100}>
+          <ScrollView style={styles.scrollView}>
+            <View style={{ minHeight: height - Constants.statusBarHeight }}>
+              <View style={{ backgroundColor: '#7a7e7f', justifyContent: 'center', minHeight: 200, }}>
+                <View style={{ alignItems: 'flex-start', top: -20, }}>
+                  <Ionicons style={{ marginLeft: 24 }} name="ios-arrow-back" size={24} color="white" />
+                </View>
+                <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', }}>
+                  <Image source={require('../../../assets/ase_nacional_imagen_app.png')} style={{ width: 180, height: 60 }} />
+                </View>
               </View>
-              <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', }}>
-                <Image source={require('../../../assets/ase_nacional_imagen_app.png')} style={{ width: 180, height: 60 }} />
+              <View style={{ backgroundColor: 'white', flex: 1, alignContent: 'center', }}>
+                <View style={{ flex: 1, alignContent: 'center', }}>
+                  <View style={{ margin: 10, alignItems: 'center' }}>
+                    <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', }}>ACTIVACIÓN DE CUENTA</Text>
+                    <Text style={{ width: '75%', margin: 10, fontSize: 14 }}>Si llegaste hasta esta pantalla es porque recibiste el link de activación de tu cuenta..</Text>
+                    <Text style={{ width: '75%', margin: 10, fontSize: 14 }}>Ingresá el código de activación que te enviamos por E-Mail.</Text>
+                  </View>
+                </View>
+                <View style={{ flex: 1, alignContent: 'center', margin: 16, }}>
+                  <Form>
+                    <Item last>
+                      <Input value={activationCode} keyboardType="numeric" onChangeText={handleActivationCodeChange} />
+                    </Item>
+                  </Form>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'flex-end', }}>
+                  <Button style={{ backgroundColor: '#F16921', margin: 32, }} disabled={disabled} onPress={() => { _textInput.setNativeProps({ height: '100%', width: '100%', opacity: 100 }); }}>
+                    <Text style={{ flex: 1, textAlign: 'center', textTransform: 'uppercase' }}>siguiente</Text>
+                  </Button>
+                </View>
               </View>
             </View>
-            <View style={{ backgroundColor: 'white', flex: 1, alignContent: 'center', }}>
-              <View style={{ flex: 1, alignContent: 'center', }}>
-              <View style={{ margin: 10, alignItems: 'center' }}>
-                <Text style={{ textAlign: 'center', margin: 10, fontWeight: 'bold', }}>ACTIVACIÓN DE CUENTA</Text>
-                <Text style={{ width: '75%', margin: 10, fontSize: 14 }}>Si llegaste hasta esta pantalla es porque recibiste el link de activación de tu cuenta..</Text>
-                <Text style={{ width: '75%', margin: 10, fontSize: 14 }}>Ingresá el código de activación que te enviamos por E-Mail.</Text>
-              </View>
-            </View>
-            <View style={{ flex: 1, alignContent: 'center', margin: 16, }}>
-                <Form>
-                  <Item last>
-                    <Input keyboardType="numeric" />
-                  </Item>
-                </Form>
-            </View>
-            <View style={{ flex: 1, justifyContent: 'flex-end', }}>
-              <Button style={{ backgroundColor: '#F16921', margin: 32, }} onPress={() => { _textInput.setNativeProps({ height: '100%', width: '100%', opacity: 100 }); }}>
-                <Text style={{ flex: 1, textAlign: 'center', textTransform: 'uppercase' }}>siguiente</Text>
-              </Button>
-            </View>
-            </View>
-          </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
-    <View ref={component => _textInput = component} style={{ flex: 1, backgroundColor: '#000000DD', position: 'absolute', left: 0, top: 0, height: '100%', width: '100%', }} onPress={() => { _textInput.setNativeProps({ height: 0, width: 0, opacity: 0 }); }}>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+      <View ref={component => _textInput = component} style={{ flex: 1, backgroundColor: '#000000DD', position: 'absolute', left: 0, top: 0, height: '100%', width: '100%', }} onPress={() => { _textInput.setNativeProps({ height: 0, width: 0, opacity: 0 }); }}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
           <View style={{ height: '75%', justifyContent: 'space-between', alignItems: 'center', alignContent: 'space-between' }}>
             <Image source={require('../../../assets/ase_nacional_imagen_app.png')} style={{ margin: 24, width: 180, height: 60 }} />
