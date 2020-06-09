3267238affff0bb92c5ed3105bb579dd3be12d37
Horacio <horacio@tdil.com.ar>
2020-06-09 16:42:14 -0300

    Init commit

--- src/components/UploadProcedure/style.js
+++ src/components/UploadProcedure/style.js
@@ -6,7 +6,7 @@ export default StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'column',
     justifyContent: 'space-around'
-  }, 
+  },
   item: {
     justifyContent: 'space-around'
   },
@@ -15,5 +15,17 @@ export default StyleSheet.create({
   },
   icon: {
     padding: 4
-  }
+  }, 
+  content: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  contentTitle: {
+    fontSize: 20,
+    marginBottom: 12,
+  },
 })
